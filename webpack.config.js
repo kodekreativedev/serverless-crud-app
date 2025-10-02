64a4e3939051f83447bc979b5834f3f90156fd2a
kodekreativedev <samian2645@gmail.com>
2025-10-02 15:19:11 -0700

    feat: Add serverless backend infrastructure

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  context: __dirname,
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  entry: slsw.lib.entries,
+  devtool: 'source-map',
+  target: 'node',
+  externals: [nodeExternals({
+    allowlist: []
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  }
+};
